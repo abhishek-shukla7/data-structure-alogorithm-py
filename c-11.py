4bf78e1349d0098ed269494995385e5bc28eef59
Abhishek shukla <abhisame077@gmail.com>
2025-12-31 16:17:34 +0530

    Hashing with the list,dictionary,string

--- c-11.py
+++ c-11.py
@@ -18,7 +18,11 @@ for num in m:
 
 #Hashing way
 
+
+
 print("\n")
+
+print("with the list ")
 
 n1=[5,3,2,2,1,5,5,7,5,10]
 
@@ -39,5 +43,53 @@ for val in m1:
 #SPACE COMPLEXITY=0(11)->O(1) ALWAYS CONSTANT
 
 
+#With the dictionary
+
+print("\n")
+print("With the dictionary")
+
+n2=[5,3,2,2,1,5,5,7,5,10]
+
+m2=[10,111,1,9,5,67,2]
+
+hash_dict={}
+
+for el1 in n2:
+    if el1 in hash_dict:
+        hash_dict[el1]+=1
+
+    else:
+        hash_dict[el1]=1
+
+
+for val2 in m2:
+    print(f"{val2} occurs {hash_dict.get(val2,0)} times")
+    
+
+#Character hashing
+
+# constrainst:
+# 1) 'a'<=s[i]<='z'
+# ascii =97-122
+
+
+print("\n")
+print("character hashing  ")
+
+s=['a','z','y','x','y','y','z','a','a','a','a']
+q=['d','a','y','x']
+
+hash_list1=[0]*123
+
+for ch in s:
+    ascii_val=ord(ch)
+    index=ascii_val-97
+    hash_list1[index]+=1
+
+
+for ch in q:
+    ascii_val=ord(ch)
+    index=ascii_val-97
+    print(f" {ch} occurs {hash_list1[index]} times")
 
 
