eaf75a6fd9f9a6323280e3befcb3d24d2c5fe4c6
Abhishek shukla <abhisame077@gmail.com>
2026-02-20 12:52:56 +0530

    Longest consecutive

--- c-39.py
+++ c-39.py
@@ -22,5 +22,38 @@ print(long(nums1))
 
 #TC=O(N2)
 #SC=o(1)
+print("\n")
+
+#better  approach
+print("better approach")
+
+def better(nums2):
+    n1=len(nums2)
+    nums2.sort()
+    count=0
+    last_smaller=float("-inf")
+    longest=0
+
+    for i in range(0,n1):
+        num1=nums2[i]
+        if num1-1==last_smaller:
+            count+=1
+            last_smaller=num1
+
+        elif num1!=last_smaller:
+            count=1
+            last_smaller=num1
+
+        longest=max(longest,count)
+
+    return longest
+
+print(better(nums1))
+
+#Tc=O(NLOGN)+O(N)=O(NlogN+N)
+#SC=O(1)
+
+
+
 
 
